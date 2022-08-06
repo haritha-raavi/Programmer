10dec79273607780a26c3542bca9949bc855f81b
RaaviHaritha <haritha5624@gmail.com>
2022-08-06 11:25:06 +0530

    Work-Done

--- Java/TechnicalRound/Slap2.java
+++ Java/TechnicalRound/Slap2.java
@@ -5,18 +5,20 @@ public class Slap2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int a[] = new int[size];// 1 2 2 3 3 3 4 4 5 5 5 5 6 6 6 7 8 9 10
-                                // 5 5 5 5 3 3 3 6 6 6 2 2 4 4 1 7 8 9 10
+        int a[] = new int[size];
         for(int i=0; i<size; i++){
             a[i]= sc.nextInt();
        }
        for(int i=0; i<size; i++){
-        if(hm.containsKey(a[i]))
-        hm.put(a[i],(hm.get(a[i])+1));
-        else
-        hm.put(a[i],1);
-
-    }
+            if(hm.containsKey(a[i]))
+            hm.put(a[i],(hm.get(a[i])+1));
+            else
+            hm.put(a[i],1);
+       }
+      for(Map.Entry<Integer, Integer> obj : hm.entrySet())
+      {
+      System.out.println( obj.getValue());
+      }
       
     }
 }
